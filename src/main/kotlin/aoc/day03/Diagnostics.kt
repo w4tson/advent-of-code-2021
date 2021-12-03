ca65a9689562585dfdda12ad52dd35b3b179add7
Paul Watson <paul.watson@pdwtech.com>
2021-12-03 21:35:09 +0000

    part2

--- src/main/kotlin/aoc/day03/Diagnostics.kt
+++ src/main/kotlin/aoc/day03/Diagnostics.kt
@@ -2,32 +2,80 @@ package aoc.day03
 
 import aoc.charList
 
+data class State(val index : Int, val inputs: List<String>)
+
 class Diagnostics(val input: String) {
+    private val diagnosticReport = input.lines()
+    
+    fun lifeSupportRating() : Int {
+        return oxygenGeneratorRating() * co2ScrubberRating()
+    }
+    
+    fun oxygenGeneratorRating() : Int {
+        return calcRating('1',::mostCommonBit)
+    }
+    
+    fun co2ScrubberRating() : Int {
+        return calcRating('0',::leastCommonBit)
+    }
+    
+    private fun calcRating(default : Char, bitCriteria : (String, Char) -> Char) : Int {
+        val res = diagnosticReport.indices.fold(State(0, diagnosticReport)) { state, _ ->
+            if (state.inputs.size > 1) {
+                val column = columnAt(state.inputs, state.index)
+                val bit = bitCriteria(column, default)
+                val filteredInputs = state.inputs.filter { it[state.index] == bit }
+                State(state.index + 1, filteredInputs)
+            } else {
+                state
+            }
+        }
+        
+        return res.inputs[0].parseBinaryInt()
+    }
     
     fun powerConsumption() : Int {
-        val gammaString = calculateGammaString(input)
+        val gammaString = calculateGammaString()
         val epsilon = calculateEpsilon(gammaString)
-        return Integer.parseInt(gammaString, 2) * epsilon
+        return gammaString.parseBinaryInt() * epsilon
     }
 
-    fun calculateGammaString(input: String): String {
-        val readings = input.lines()
-
-        val gamma = (0..readings[0].length - 1)
-            .map { mostCommonBit(columnAt(readings, it)) }
+    fun calculateGammaString(): String {
+        return (diagnosticReport[0].indices)
+            .map { mostCommonBit(columnAt(diagnosticReport, it)) }
             .joinToString("")
-        return gamma
     }
     
     fun calculateEpsilon(input: String) : Int {
-        return Integer.parseInt(input.replace('1', 'x').replace('0','1').replace('x','0'), 2)
+        return input.replace('1', 'x')
+                .replace('0','1')
+                .replace('x','0')
+                .parseBinaryInt()
     }
      
     fun columnAt(input: List<String>, index: Int) : String {
         return input.map { it[index] }.joinToString("")
     }
-    
-    fun mostCommonBit(binaryString : String) : Char {
-        return if (binaryString.charList().filter { it == '1' }.count() > (binaryString.length / 2)) '1' else '0'
+}
+
+fun mostCommonBit(binaryString : String, default : Char = '0') : Char {
+    val counts = binaryString.charList().groupingBy { it  }.eachCount()
+    return if (counts['1'] == counts['0']) {
+        default
+    } else {
+        counts.maxBy { it.value }!!.key
     }
+}
+
+fun leastCommonBit(binaryString: String, default: Char = '0') : Char {
+    val counts = binaryString.charList().groupingBy { it  }.eachCount()
+    return if (counts['1'] == counts['0']) {
+        default
+    } else {
+        counts.minBy { it.value }!!.key
+    }
+}
+    
+fun String.parseBinaryInt() : Int {
+    return Integer.parseInt(this, 2)
 }
--- src/test/kotlin/aoc/day03/DiagnosticsTest.kt
+++ src/test/kotlin/aoc/day03/DiagnosticsTest.kt
@@ -7,14 +7,13 @@ import org.junit.Test
 class DiagnosticsTest {
     val exampleInput = Util.readInput("/day03/example.txt")
     val input = Util.readInput("/day03/input.txt")
-
     
     @Test
     fun example() {
         val d = Diagnostics(exampleInput)
-        assertEquals('1', d.mostCommonBit("11100"))
-        assertEquals('0', d.mostCommonBit("1110000"))
-        assertEquals("10110", d.calculateGammaString(exampleInput))
+        assertEquals('1', mostCommonBit("11100"))
+        assertEquals('0', mostCommonBit("1110000"))
+        assertEquals("10110", d.calculateGammaString())
         assertEquals(9, d.calculateEpsilon("10110"))
         val powerConsumption = d.powerConsumption()
         assertEquals(198, powerConsumption)
@@ -25,5 +24,27 @@ class DiagnosticsTest {
         val d = Diagnostics(input)
         val consumption = d.powerConsumption()
         println(consumption)
+    }
+
+    @Test
+    fun example2() {
+        val d = Diagnostics(exampleInput)
+        val oxygenGeneratorRating = d.oxygenGeneratorRating()
+        val co2ScrubberRating = d.co2ScrubberRating()
+        assertEquals(23, oxygenGeneratorRating)
+        assertEquals(10, co2ScrubberRating)
+    }
+
+    @Test
+    fun testCommonFuncs() {
+        val leastCommonBit = leastCommonBit("011110011100")
+        val mostCommon = mostCommonBit("000", '1')
+        assertEquals('0', leastCommonBit)
+        assertEquals('0', mostCommon)
+    }
+
+    @Test
+    fun part2() {
+        println(Diagnostics(input).lifeSupportRating())
     }
 }
